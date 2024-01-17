ebc7fa0ea1f2449e17916be9bdf96ea1b2283723
Linfindel <116459177+linfindel@users.noreply.github.com>
2024-01-17 15:30:38 +0000

    Improved mobile support

---
+++ mobile.js
@@ -0,0 +1,23 @@
+setInterval(() => {
+    if (screen.width < screen.height) {
+        document.getElementById("pfp").className = "card-flat-bottom";
+        document.getElementById("pfp").style.width = "100%";
+        document.getElementById("pfp").style.height = "20rem";
+        document.getElementById("text").style.width = "100%";
+        document.getElementById("button-column").className = "row-flat";
+        document.getElementById("button-column").style.height = "";
+        document.getElementById("button-column").style.width = "100%";
+        document.getElementById("gh-button").className = "card-flat-bottom-left-alt";
+    }
+
+    else {
+        document.getElementById("pfp").className = "card-flat-right";
+        document.getElementById("pfp").style.width = "10rem";
+        document.getElementById("pfp").style.height = "10rem";
+        document.getElementById("text").style.width = "";
+        document.getElementById("button-column").className = "column-flat";
+        document.getElementById("button-column").style.height = "240px";
+        document.getElementById("button-column").style.width = "";
+        document.getElementById("gh-button").className = "card-flat-top-right-alt";
+    }
+});
