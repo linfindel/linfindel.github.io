cd0bf1e168f5119b7d7f8dfcd7c783b6928c782d
Cirilla <116459177+linfindel@users.noreply.github.com>
2025-01-22 15:12:43 +0000

    Rework links

--- mobile.js
+++ mobile.js
@@ -4,10 +4,8 @@ setInterval(() => {
         document.getElementById("pfp").style.width = "100%";
         document.getElementById("pfp").style.height = "20rem";
         document.getElementById("text").style.width = "100%";
-        document.getElementById("button-column").className = "row-flat";
-        document.getElementById("button-column").style.height = "";
-        document.getElementById("button-column").style.width = "100%";
-        document.getElementById("gh-button").className = "card-flat-bottom-left-alt";
+        document.getElementById("text").style.alignItems = "";
+        document.getElementById("text").className = "card-flat-top column";
     }
 
     else {
@@ -15,9 +13,7 @@ setInterval(() => {
         document.getElementById("pfp").style.width = "10rem";
         document.getElementById("pfp").style.height = "10rem";
         document.getElementById("text").style.width = "";
-        document.getElementById("button-column").className = "column-flat";
-        document.getElementById("button-column").style.height = "240px";
-        document.getElementById("button-column").style.width = "";
-        document.getElementById("gh-button").className = "card-flat-top-right-alt";
+        document.getElementById("text").style.alignItems = "flex-start";
+        document.getElementById("text").className = "card-flat-left column";
     }
 });
